cdbf9a3bca5a70c4fb0dae5d6cb6e58807c06081
Marius Mandal <mariusmandal@gmail.com>
2020-03-13 11:37:12 +0100

    Optimalisert arrangementinfo

--- Controller/Norge/avlyst.php
+++ Controller/Norge/avlyst.php
@@ -1,7 +1,9 @@
 <?php
 
+use UKMNorge\Arrangement\Load;
 use UKMNorge\DesignWordpress\Environment\Wordpress;
 use UKMNorge\Geografi\Fylker;
 
 Wordpress::setView('Norge/Avlyst');
 Wordpress::addViewData('fylker', Fylker::getAll());
+Wordpress::addViewData('arrangementLoader', new Load());
