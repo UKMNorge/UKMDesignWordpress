932bcd21f5708f1b8ee491a89c2f8c8fab786ef1
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-06-09 16:03:56 +0200

    Ny struktur festival-nettside
    Ny struktur av festival-nettsiden med nye sider og ny innhold.

---
+++ Controller/Festivalen/deltaker-publikum.php
@@ -0,0 +1,7 @@
+<?php
+
+use UKMNorge\Design\UKMDesign;
+use UKMNorge\DesignWordpress\Environment\Posts;
+use UKMNorge\DesignWordpress\Environment\Wordpress;
+
+Wordpress::setView('Festivalen/Front/DeltakerPublikum');
