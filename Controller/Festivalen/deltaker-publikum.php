d6cef29efc1d1eadc5f8eccf31d328db505a27cb
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2022-06-14 14:19:55 +0200

    Nyheter på første side

--- Controller/Festivalen/deltaker-publikum.php
+++ Controller/Festivalen/deltaker-publikum.php
@@ -4,4 +4,13 @@ use UKMNorge\Design\UKMDesign;
 use UKMNorge\DesignWordpress\Environment\Posts;
 use UKMNorge\DesignWordpress\Environment\Wordpress;
 
+UKMDesign::getHeader()->hideSectionTitle();
+UKMDesign::getHeader()->getLogo()->hide();
+
+Wordpress::setView('Festivalen/Front/Underveis');
+
+// Get post for current year
+$posts = Posts::getByYear(date("Y"));
+Wordpress::setPosts($posts);
+
 Wordpress::setView('Festivalen/Front/DeltakerPublikum');
