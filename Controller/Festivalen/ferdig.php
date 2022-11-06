2a19ee1567793d7dd99831f15e78ec8912828370
Jardar Nordbø <jardar@ukm.no>
2022-11-06 19:56:36 +0100

    Controller for ferdig side for festivalen

---
+++ Controller/Festivalen/ferdig.php
@@ -0,0 +1,16 @@
+<?php
+
+use UKMNorge\Design\UKMDesign;
+use UKMNorge\DesignWordpress\Environment\Posts;
+use UKMNorge\DesignWordpress\Environment\Wordpress;
+
+UKMDesign::getHeader()->hideSectionTitle();
+UKMDesign::getHeader()->getLogo()->hide();
+
+Wordpress::setView('Festivalen/Front/Underveis');
+
+// Get post for current year
+$posts = Posts::getByYear(date("Y"));
+Wordpress::setPosts($posts);
+
+Wordpress::setView('Festivalen/Front/Ferdig');
