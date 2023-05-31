6c9ee49f8c84b92704e7a94e2c84bf5c544de006
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-05-31 13:53:22 +0200

    Program for tekniske prøver

---
+++ Controller/Festivalen/program-tekniskprove.php
@@ -0,0 +1,44 @@
+<?php
+
+use UKMNorge\Arrangement\Arrangement;
+use UKMNorge\Arrangement\Program\Hendelser;
+use UKMNorge\Design\UKMDesign;
+use UKMNorge\DesignWordpress\Environment\Post;
+use UKMNorge\DesignWordpress\Environment\Posts;
+use UKMNorge\DesignWordpress\Environment\Wordpress;
+use UKMNorge\Filmer\UKMTV\Direkte\Sendinger;
+
+$arrangement = new Arrangement( get_option('pl_id') );
+
+Wordpress::addViewData('arrangement',$arrangement);
+
+## Skal hente ut programmet for en forestilling
+$id = Wordpress::getLastParameter();
+
+
+$visInterne = defined('DELTAKERPROGRAM') && DELTAKERPROGRAM;
+$hendelser = $visInterne ? $arrangement->getProgram()->getAllInkludertInterne() : $arrangement->getProgram()->getAll();
+
+$gyldigeHendelser = [];
+
+// OBS!!!!!! 
+// Her sjekker vi navn av hendelsen. Kan være farlig fordi navnet kan inneholde "Teknisk prøver" hvor som helst
+foreach($hendelser as $hendelse) {
+    if(strpos($hendelse->navn, 'Teknisk prøve') !== false || strpos($hendelse->navn, 'Tekniske prøver') !== false) {
+        $gyldigeHendelser[] = $hendelse;    
+    }
+}
+
+# Skal vise program for arrangementet
+$program = Hendelser::sorterPerDag( $gyldigeHendelser );
+
+
+Wordpress::setView('Arrangement/Program/Oversikt');
+
+Wordpress::addViewData(
+    [
+        'visInterne' =>$visInterne,
+        'program' => $program,
+        'isUKMFestivalen' => true
+    ]
+);
