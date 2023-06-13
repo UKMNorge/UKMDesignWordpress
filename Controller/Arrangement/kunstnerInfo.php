c9b0620f171d45c58694733297db3fa2aa39fe63
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-06-13 14:25:54 +0200

    Kunstner info lenke
    Denne lenken skal brukes for å representere informasjon på kunstnere. I tillegg kobles denne siden mot WP post med spesifikk kategori og innslag_id

---
+++ Controller/Arrangement/kunstnerInfo.php
@@ -0,0 +1,53 @@
+<?php
+
+use UKMNorge\Arrangement\Arrangement;
+use UKMNorge\DesignWordpress\Environment\Wordpress;
+use UKMNorge\Innslag\Innslag;
+use UKMNorge\DesignWordpress\Environment\Posts;
+
+
+Wordpress::setView('Arrangement/Kunstner-info.htm.twig');
+$arrangement = new Arrangement(get_option('pl_id'));;
+
+
+if (isset($_GET['hendelse'])) {
+    Wordpress::addViewData('vis', $_GET['hendelse']);
+}
+
+$id = Wordpress::getLastParameter();
+$innslag = null;
+try{
+    $innslag = Innslag::getById($id);
+}catch(Exception $e) {
+    // Ikke nødvendigvis noe feil. Lenken kan inneholde ugyldig id for innslag
+}
+
+
+// Kategorien kunstnere-qr-kode brukes for å identifisere posts som tilhører til kunstnere
+$catid = get_category_by_slug('kunstnere-qr-kode')->term_id;
+
+// Hent alle posts på kategori
+$posts = Posts::getByCategory($catid);
+$posts->paged = 0;
+$posts->setPostsPerPage(200);
+$posts->loadPosts();
+
+$post = null;
+if($innslag) {
+    foreach($posts->posts as $p) {
+        // Sjekker tilpassede felt for innslag_id
+        $tilpassedeFelt = get_post_meta($p->ID, 'innslag_id', true);
+        if(strval($innslag->getId()) == $tilpassedeFelt) {
+            $post = $p;
+            continue;
+        }
+    }
+}
+
+Wordpress::addViewData(
+    [
+        'innslag' => $innslag,
+        'arrangement' => $arrangement,
+        'post' => $post
+    ]
+);
