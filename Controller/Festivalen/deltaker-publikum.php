56f127633c1a7652d2b64c504c25eb3558a5ab9f
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2023-06-22 00:09:02 +0200

    Første versjon av hva skjer på liveprogram

--- Controller/Festivalen/deltaker-publikum.php
+++ Controller/Festivalen/deltaker-publikum.php
@@ -3,6 +3,10 @@
 use UKMNorge\Design\UKMDesign;
 use UKMNorge\DesignWordpress\Environment\Posts;
 use UKMNorge\DesignWordpress\Environment\Wordpress;
+use UKMNorge\Twig\Twig;
+use UKMNorge\Arrangement\Arrangement;
+use UKMNorge\Arrangement\Program\Hendelser;
+
 
 UKMDesign::getHeader()->hideSectionTitle();
 UKMDesign::getHeader()->getLogo()->hide();
@@ -16,8 +20,78 @@ $posts->paged = 0;
 $posts->setPostsPerPage(200);
 $posts->loadPosts();
 
+$arrangement = new Arrangement( get_option('pl_id') );
+
+$hendelser = $visInterne ? $arrangement->getProgram()->getAllInkludertInterne() : $arrangement->getProgram()->getAll();
+$program = Hendelser::sorterPerDag( $hendelser );
+
+
+$hendelserDato = [];
+foreach ($program as $p) {
+    foreach($p->forestillinger as $f) {
+        $hendelserDato[$f->getStart()->getTimestamp()][] = $f;
+    }
+}
+
+$filter = function ($datetime) {
+    $passed = time() > strtotime($datetime);
+    $time = abs(time() - strtotime($datetime)); 
+    
+    if($time > 3600) {
+        // return $datetime;
+    }
+    
+    $units = array (
+        3600 => 'time',
+        60 => 'minutt',
+        1 => 'sekund'
+    );
+  
+    foreach ($units as $unit => $val) {
+        if ($time < $unit) continue;
+        
+        $numberOfUnits = abs(floor($time / $unit));
+
+        $ret = $passed ? 'Startet for ca. ' : 'Om ca. ';
+        $retSek = $passed ? 'Startet for ' : 'Starter Om ';
+        
+
+        if($val == 'sekund') {
+            $ret = $retSek . 'noen sekunder';
+        }
+        else if($val == 'minutt') {
+            $ret = $ret . $numberOfUnits . ' minutt' . (($numberOfUnits > 1) ? 'er' : '');
+        }
+        else if($val == 'time') {
+            if($numberOfUnits > 23 || !$passed) {
+                $date = new DateTime($datetime);
+
+                return $date->format('d.m H:i');
+            }
+            $ret = $ret . $numberOfUnits . ' time' . (($numberOfUnits > 1) ? 'r' : '');
+        }
+        
+        return $passed ? $ret . ' siden' : $ret;
+    }
+};
+
+Twig::addFilter('timeago', $filter);
 
 Wordpress::setPosts($posts);
 
+
+function find_closest($array, $date) {
+    foreach($array as $hendelse) {   
+        $interval[] = ['value' => abs(strtotime($date) - strtotime($hendelse[0]->getStart()->format('Y-m-d H:i:s'))), 'hendelse' => $hendelse[0]];
+    }
+    asort($interval);
+    return $interval;
+}
+
+$henselserSorter = find_closest($hendelserDato, date("Y-m-d H:i:s"));
+
+Wordpress::addViewData([
+    'henselserSorter' => $henselserSorter
+]);
 
 Wordpress::setView('Festivalen/Front/DeltakerPublikum');
