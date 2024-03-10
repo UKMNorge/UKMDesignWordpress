3bc0410ba390fae569302fe4a7053df9a6179268
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-03-11 00:14:56 +0100

    CSS må komme først

--- Controller/Norge/arrangement_finder.php
+++ Controller/Norge/arrangement_finder.php
@@ -4,9 +4,9 @@ use UKMNorge\DesignWordpress\Environment\Wordpress;
 use UKMNorge\Geografi\Fylker;
 
 // wp_enqueue_script( 'build-js', get_template_directory_uri() . 'Client/arrangement_finder/dist/assets/build.js', array(), '1.0.0', true );
+wp_enqueue_style('UKMvideoArrSysStyle', '//assets.' . UKM_HOSTNAME . '//css/arr-sys.css');
 wp_enqueue_script('custom-script', get_template_directory_uri() . '/Client/arrangement_finder/dist/assets/build.js', array('jquery'));
 wp_enqueue_style('custom-style', get_template_directory_uri() . '/Client/arrangement_finder/dist/assets/build.css', array());
-wp_enqueue_style('UKMvideoArrSysStyle', '//assets.' . UKM_HOSTNAME . '//css/arr-sys.css');
 
 Wordpress::setView('Norge/Arrangement_finder');
 Wordpress::addViewData('fylker', Fylker::getAll());
